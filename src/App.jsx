f2a621eaf5bd1dfced22e067ab366b3b4720b7c4
Nehbi-Ramadani <nehbiramadani2023@gmail.com>
2023-12-13 14:44:16 +0100

    RumList-AlkFreiList fertig

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.scss";
+import AlkFrei from "./components/alkFrei/AlkFrei"
+import Rum from "./components/rum/Rum"
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/rum" element={<Rum/>}/>
+        <Route path="/alkFrei" element={<AlkFrei/>}/>
+      </Routes>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import Header from "../components/headerComponents/Header";
 import Footer from "../components/footerComponents/Footer";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
       <Header />
       <main>
+        <Link to="/rum">Rum</Link>
+        <Link to="/alkFrei">Alkohol Freie</Link>
         <p>HIER KOMMT DIE MAIN SECTION</p>
       </main>
       <Footer />
