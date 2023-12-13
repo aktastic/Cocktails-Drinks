2b5ef42ea7c939a809dc4a31cec3f0be2c9659ca
aktastic <arkulac@gmail.com>
2023-12-13 15:18:57 +0100

    small changes to make it work

--- src/components/headerComponents/SearchItem.jsx
+++ src/components/headerComponents/SearchItem.jsx
@@ -1,16 +1,17 @@
-
-import './SearchItem.scss'
-
+import "./SearchItem.scss";
 
 const SearchItem = (props) => {
+  console.log(props.singleDrinkObj.idDrink);
 
-    return (  
-        <article key={props.key} className="singleSearchItem_wrap">
-            <img src={props.img} alt={props.alt} />
-            <h3>{props.drinkName}</h3>
-        </article>
-                
-    )
-}
- 
+  return (
+    <article
+      key={props.singleDrinkObj.idDrink}
+      className="singleSearchItem_wrap"
+    >
+      <img src={props.img} alt={props.alt} />
+      <h3>{props.drinkName}</h3>
+    </article>
+  );
+};
+
 export default SearchItem;
